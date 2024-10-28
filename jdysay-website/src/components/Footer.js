ecd818584e1287f03213968eb44a0d80a0b431c0
jdysay <jdysay@icloud.com>
2024-10-28 14:55:53 -0700

    updated footer font and style

--- jdysay-website/src/components/Footer.js
+++ jdysay-website/src/components/Footer.js
@@ -4,10 +4,8 @@ import './Footer.css';
 function Footer() {
     return (
         <footer className="footer">
-            <p>© 2023 Jaycie Say</p>
-            <div className="contact-info">
-                <p>jds34@sfu.ca</p>
-            </div> 
+            <p>© 2024 Jaycie Say</p>
+           <p>jds34@sfu.ca</p>
         </footer>
     )
 }
