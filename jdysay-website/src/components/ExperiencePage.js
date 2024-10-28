9734cf79c71d0aa1ea4372bce34ea37b6db5ca13
jdysay <jdysay@icloud.com>
2024-10-28 08:16:00 -0700

    added header to main page

---
+++ jdysay-website/src/components/ExperiencePage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './Header';
+
+function MainPage() {
+    return (
+        <div>
+            <Header title="Jaycie Say">
+                <main>
+                    <p>Bio</p>
+                </main>
+            </Header>
+        </div>
+    );
+}
+
+export default MainPage;
