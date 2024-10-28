f34d87dfef2cceafd3d8f3e00ccf9964974556d6
jdysay <jdysay@icloud.com>
2024-10-28 15:03:06 -0700

    created BioComponent for main page

---
+++ jdysay-website/src/components/BioContent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './BioContent.css';
+
+function MainContent() {
+    return (
+        <main className="main-content">
+            <h2>Hi!</h2>
+            <p>[bio content]</p>
+        </main>
+    );
+}
