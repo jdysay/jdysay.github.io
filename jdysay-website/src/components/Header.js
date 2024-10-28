45265601bca59b77d80de010ad38d3a959bef68e
jdysay <jdysay@icloud.com>
2024-10-28 09:03:33 -0700

    updated navigation bar to Header component

--- jdysay-website/src/components/Header.js
+++ jdysay-website/src/components/Header.js
@@ -4,6 +4,13 @@ function Header({title}) {
     return (
         <header className="header"> 
             <h1>{title}</h1>
+            <nav>
+               <ul>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/ExperiencePage">Experience</Link></li>
+                    <li><Link to="/ProjectPage">Projects</Link></li>
+               </ul>
+            </nav>
         </header>
     );
 }
