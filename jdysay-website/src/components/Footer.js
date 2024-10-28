1cd00193c950a59b52e7bc8dcafbd3c79ef25e18
jdysay <jdysay@icloud.com>
2024-10-28 14:47:21 -0700

    added footer component

---
+++ jdysay-website/src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+    return (
+        <footer className="footer">
+            <p>© 2023 Jaycie Say</p>
+            <div className="contact-info">
+                <p>jds34@sfu.ca</p>
+            </div> 
+        </footer>
+    )
+}
