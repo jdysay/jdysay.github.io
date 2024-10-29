7c10e8c9922e8e6f170f6b4e49c87b367bb47dac
jdysay <jdysay@icloud.com>
2024-10-28 17:45:17 -0700

    update information on experience page

--- jdysay-website/src/pages/ExperiencePage.js
+++ jdysay-website/src/pages/ExperiencePage.js
@@ -8,11 +8,15 @@ function ExperiencePage() {
             <div className="experience-text">
                 <h2>Work Experience</h2>
                 <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et 
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip 
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu 
-                    fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt 
-                    mollit anim id est laborum.
+                    <strong>
+                        On-Call Activity Leader, &nbsp;
+                        <a href="https://www.gv.ymca.ca" target="_blank" rel="noopener noreferrer">YMCA</a>
+                    </strong> 2024 - present 
+                    
+                    <br></br>
+                    
+                    <strong>Group Leader, YMCA</strong> 2023 <br></br>
+                    <strong>Early Learner Assistant, Kumon</strong> 2022 - 2023
                 </p>
                 <h2>Volunteer Experience</h2>
                 <p>
