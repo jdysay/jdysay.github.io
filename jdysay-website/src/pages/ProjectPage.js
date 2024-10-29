db70c3cdfc8211e60a352b8cad3993858f494d7a
jdysay <jdysay@icloud.com>
2024-10-28 21:07:42 -0700

    added image to projects page

--- jdysay-website/src/pages/ProjectPage.js
+++ jdysay-website/src/pages/ProjectPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from '../components/Header';
 import './Project.css';
+import projectImage from './images/umi.png'
 
 function ProjectPage() {
     return (
@@ -8,27 +9,15 @@ function ProjectPage() {
             <div className="project-text">
                 <h2>Current Projects</h2>
                 <p>
-                    [update when more projects are created] 
-                    <br></br>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et 
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip 
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu 
-                    fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt 
-                    mollit anim id est laborum.
+                   <strong>Umi</strong> - task management and music all in one to help student increase focus and productivity!
                 </p>
                 <h2>Past Projects</h2>
                 <p>
                     [update when more projects are created] 
-                    <br></br>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et 
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip 
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu 
-                    fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt 
-                    mollit anim id est laborum.
                 </p>
             </div>
             <div className="images">
-                <p>images</p>
+                <img src={projectImage} alt="image of our project icon"></img>
             </div>
         </div>
     );
