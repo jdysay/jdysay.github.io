66704428984052fbd6983953cfbdaf6a39470677
jdysay <jdysay@icloud.com>
2024-10-28 21:37:38 -0700

    edited code for clarity

--- jdysay-website/src/App.js
+++ jdysay-website/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import {  BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
