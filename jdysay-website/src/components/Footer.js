e3dd03c9585bd846e2223c68eb359db7709969b9
jdysay <jdysay@icloud.com>
2024-10-28 21:25:38 -0700

    updated footer

--- jdysay-website/src/components/Footer.js
+++ jdysay-website/src/components/Footer.js
@@ -4,7 +4,7 @@ import './Footer.css';
 function Footer() {
     return (
         <footer className="footer">
-            <p>© 2024 Jaycie Say</p>
+            <p>© 2024 jdysay</p>
         </footer>
     );
 }
