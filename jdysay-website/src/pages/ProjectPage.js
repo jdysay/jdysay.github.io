38b7c37d9959eb5e75e2c7f7b4c6425853e2d63e
jdysay <jdysay@icloud.com>
2025-01-14 09:55:47 -0800

    updated project page

--- jdysay-website/src/pages/ProjectPage.js
+++ jdysay-website/src/pages/ProjectPage.js
@@ -8,11 +8,21 @@ function ProjectPage() {
             <div className="project-text">
                 <h2>Current Projects</h2>
                 <p>
-                   <strong>Umi</strong> - task management and music all in one to help student increase focus and productivity!
+                  
                 </p>
                 <h2>Past Projects</h2>
                 <p>
-                    [update when more projects are created] 
+                <strong>Umi</strong> - task management and music all in one to help student increase focus and productivity!
+                </p>
+                <p>
+                Collaborated on the development of a task management web application for students
+                Utilized a Work Breakdown Structure (WBS) to allocate tasks among team members, ensuring clarity and efficient progress.
+                Developed a project schedule with milestones, deadlines, and deliverables to guide the team's workflow.
+                Regularly monitored progress to align with the project timeline and Kanban board, ensuring on-time delivery.
+                Developed core functionality for task management using the Notion API, enabling users to add, edit, and delete tasks directly from the platform.
+                Designed and implemented seamless navigation and flow across all pages, including the home and FAQ pages.
+                Implemented CI/CD pieline for smooth Automatic Testing and Deployment
+                Assisted team members in debugging and resolving issues to ensure smooth project progress and functionality.
                 </p>
             </div>
             <div className="images">
