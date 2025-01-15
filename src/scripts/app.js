ff747caa818218e277e0d3b8402761cb5e647acc
jdysay <jdysay@icloud.com>
2025-01-15 14:41:43 -0800

    updated website to vanilla-js

---
+++ src/scripts/app.js
@@ -0,0 +1,16 @@
+// scripts/app.js
+
+// Populate the header
+document.getElementById('header').innerHTML = `
+  <h1>Jaycie Say</h1>
+  <nav>
+    <a href="index.html">Home</a>
+    <a href="experience.html">Experience</a>
+    <a href="projects.html">Projects</a>
+  </nav>
+`;
+
+// Populate the footer
+document.getElementById('footer').innerHTML = `
+  <p>&copy; 2025 Jaycie Say</p>
+`;
